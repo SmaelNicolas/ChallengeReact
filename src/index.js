7da4d47b927e861f8e5abf0ae57d3b0b3faaaab0
Smael Nicolas <nico.smael@gmail.com>
2022-04-10 12:30:08 -0300

    add bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Index.css";
 
 const container = document.getElementById("root");
