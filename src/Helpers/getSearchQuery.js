26e5a01fb536f59e60a1073dd5414b5e0f879c9e
Smael Nicolas <nico.smael@gmail.com>
2022-04-11 19:30:35 -0300

    get list of autocomplete recipes

---
+++ src/Helpers/getSearchQuery.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const doSearchQuery = async (value, fn) => {
+	let result;
+	await axios({
+		method: "GET",
+		url: `https://api.spoonacular.com/recipes/autocomplete?number=15&query=${value}&apiKey=0aa9b30d89cc43f3968c6c1321dee967 `,
+	}).then((response) => {
+		fn(response.data);
+	});
+	return result;
+};
+
+export default doSearchQuery;
