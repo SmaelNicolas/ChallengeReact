e4aa3a80c7bd671fd9035bae020e6c0543c2dc63
Smael Nicolas <nico.smael@gmail.com>
2022-04-11 19:31:38 -0300

    card dish to test

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import CardDish from "../../Components/CardDish/CardDish";
+import Title from "../../Components/Title/Title";
+import "./Home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	useEffect(() => {
+		//fetch url
+		const fetchData = async () => {
+			const result = await axios(
+				"https://api.spoonacular.com/recipes/complexSearch?apiKey=0aa9b30d89cc43f3968c6c1321dee967"
+			);
+			console.log(result);
+		};
+
+		// fetchData();
+	}, []);
+
+	return (
+		<div className='homeContainer'>
+			<Title title={"MENU"} />
+			<CardDish />
+		</div>
+	);
 };
 
 export default Home;
