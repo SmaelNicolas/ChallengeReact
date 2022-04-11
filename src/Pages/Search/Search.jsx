4ec7e3e6a934ef44e37e2b2acd8982e0ad97ab9c
Smael Nicolas <nico.smael@gmail.com>
2022-04-11 19:29:43 -0300

    search input for recipes

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+import { Button } from "react-bootstrap";
+
+import "./Search.css";
+import doSearchQuery from "../../Helpers/getSearchQuery";
+import RecipesSearched from "./RecipesSearched/RecipesSearched";
+import createCompleteRecipe from "../../Helpers/createCompleteRecipe";
 
 const Search = () => {
-	return <div>Search</div>;
+	const [recipes, setRecipes] = useState(undefined);
+	const [completeRecipes, setCompleteRecipes] = useState(undefined);
+
+	const SignupSchema = Yup.object().shape({
+		querySearch: Yup.string()
+			.min(3, "Too Short!")
+			.max(25, "Too Long!")
+			.required("Required"),
+	});
+
+	const addRecipes = (value) => {
+		setRecipes(value);
+	};
+	const addCompleteRecipe = (value) => {
+		setCompleteRecipes(value);
+	};
+
+	useEffect(() => {
+		createCompleteRecipe(recipes, addCompleteRecipe);
+	}, [recipes]);
+
+	return (
+		<>
+			<Formik
+				initialValues={{
+					querySearch: "",
+				}}
+				validationSchema={SignupSchema}
+				onSubmit={(values) => {
+					doSearchQuery(values.querySearch, addRecipes);
+				}}
+			>
+				{({ errors, touched }) => (
+					<Form className='formSearchContainer'>
+						<label htmlFor='querySearch'>
+							What are you looking for
+						</label>
+						<Field name='querySearch' />
+						{errors.querySearch && touched.querySearch ? (
+							<div>{errors.querySearch}</div>
+						) : null}
+						<Button variant='success' type='submit'>
+							Submit
+						</Button>
+					</Form>
+				)}
+			</Formik>
+			{completeRecipes !== undefined && (
+				<RecipesSearched recipes={recipes} />
+			)}
+		</>
+	);
 };
 
 export default Search;
