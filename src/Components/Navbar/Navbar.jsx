75e649b81d8bc885e4e24fcdae44d3b1d756d613
Smael Nicolas <nico.smael@gmail.com>
2022-04-11 19:31:13 -0300

    add router to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-	return <div className='navbarContainer'>Navbar</div>;
+	return (
+		<div className='navbarContainer'>
+			<ul>
+				<li>
+					<Link to='/'>LOGIN</Link>
+				</li>
+				<li>
+					<Link to='/home'>HOME</Link>
+				</li>
+				<li>
+					<Link to='/search'>SEARCH</Link>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navbar;
