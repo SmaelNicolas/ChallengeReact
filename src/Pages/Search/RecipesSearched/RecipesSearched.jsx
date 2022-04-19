d13a30b02f1b558178bbb38dd6a3b19304092923
Smael Nicolas <nico.smael@gmail.com>
2022-04-19 00:32:54 -0300

    handle quantity items added

--- src/Pages/Search/RecipesSearched/RecipesSearched.jsx
+++ src/Pages/Search/RecipesSearched/RecipesSearched.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CardDish from "../../../Components/CardDish/CardDish";
+import { MenuContext } from "../../../Context/MenuContext";
 import "./RecipesSearched.css";
 
 const RecipesSearched = ({ recipes }) => {
 	console.log(recipes);
+
+	const { addRecipe, canAddVegan, canAddNoVegan } = useContext(MenuContext);
+	const [addVegan, setAddVegan] = useState(true);
+	const [addNoVegan, setAddNoVegan] = useState(true);
+
+	const checkCanAddVegan = (recipe) => {
+		canAddVegan() < 2 && addRecipe(recipe);
+		canAddVegan() === 2 && setAddVegan(false);
+	};
+
+	const checkCanAddNoVegan = (recipe) => {
+		canAddNoVegan() < 2 && addRecipe(recipe);
+		canAddNoVegan() === 2 && setAddNoVegan(false);
+	};
+
+	useEffect(() => {}, [recipes, addNoVegan, addVegan]);
+
 	return (
 		<>
 			<h1>Recipes Searched</h1>
-			<div className='recipesSearchedContainer'>
+			<div className='recipesSearchedContainer' key='recipeContainer'>
 				{recipes.map((recipe) => (
 					<CardDish
+						recipe={recipe}
 						key={recipe.id}
 						title={recipe.title}
 						img={recipe.img}
 						desc={recipe.description}
 						add={true}
+						addVegan={addVegan}
+						addNoVegan={addNoVegan}
+						checkCanAddVegan={checkCanAddVegan}
+						checkCanAddNoVegan={checkCanAddNoVegan}
 					/>
 				))}
 			</div>
