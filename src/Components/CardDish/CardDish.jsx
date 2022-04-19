fd660c776e3e8a478a1d2541b33f773e12f2d88f
Smael Nicolas <nico.smael@gmail.com>
2022-04-19 00:31:42 -0300

    add more props , disable button opts

--- src/Components/CardDish/CardDish.jsx
+++ src/Components/CardDish/CardDish.jsx
@@ -3,28 +3,68 @@ import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import "./CardDish.css";
 
-const CardDish = ({ key, title, img, desc, add }) => {
+const CardDish = ({
+	recipe,
+	key,
+	title,
+	img,
+	desc,
+	add,
+	addVegan,
+	addNoVegan,
+	hs,
+	price,
+	time,
+	deleteRecipe,
+	checkCanAddVegan,
+	checkCanAddNoVegan,
+}) => {
 	return (
-		<Card className='cardDish' key={key}>
-			<Card.Title variant='top'>{title}</Card.Title>
-			<Card.Img src={img} />
-			<Card.Body>
-				<Card.Text className='cardDescription'>{desc}</Card.Text>
-				<Button className='cardButton' variant='info'>
-					Details
-				</Button>
-				{!add && (
-					<Button className='cardButton' variant='danger'>
-						Delete
-					</Button>
+		<>
+			<Card className='cardDish' key={key}>
+				<Card.Title variant='top'>{title}</Card.Title>
+				<Card.Img src={img} />
+				{hs !== undefined && (
+					<div className='cardText'>
+						<p>Healt Score {hs}</p>
+						<p>Price {price.toFixed(2)}</p>
+						<p>time of preparation {time}</p>
+					</div>
 				)}
-				{add && (
-					<Button className='cardButton' variant='success'>
-						Add
+
+				<Card.Body>
+					<Card.Text className='cardDescription'>{desc}</Card.Text>
+
+					<Button className='cardButton' variant='info'>
+						Details
 					</Button>
-				)}
-			</Card.Body>
-		</Card>
+					{!add && (
+						<Button
+							className='cardButton'
+							variant='danger'
+							onClick={() => deleteRecipe(recipe)}
+						>
+							Delete
+						</Button>
+					)}
+					{recipe.vegan
+						? addVegan
+						: addNoVegan && (
+								<Button
+									className='cardButton'
+									variant='success'
+									onClick={() =>
+										recipe.vegan
+											? checkCanAddVegan(recipe)
+											: checkCanAddNoVegan(recipe)
+									}
+								>
+									Add
+								</Button>
+						  )}
+				</Card.Body>
+			</Card>
+		</>
 	);
 };
 
