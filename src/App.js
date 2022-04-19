23698400cd283367f0be554c6d939ea9b00a712a
Smael Nicolas <nico.smael@gmail.com>
2022-04-18 21:21:54 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Search from "./Pages/Search/Search";
+import Details from "./Pages/Details/Details";
+import { MenuProvider } from "./Context/MenuContext";
 
 const App = () => {
 	return (
-		<div className='appContainer'>
-			<Navbar />
-			<Login />
-		</div>
+		<MenuProvider>
+			<BrowserRouter>
+				<div className='appContainer'>
+					<Navbar />
+					<Routes>
+						<Route exact path='/' element={<Login />} />
+						<Route exact path='/home' element={<Home />} />
+						<Route exact path='/search' element={<Search />} />
+						<Route exact path='/detail/:id' element={<Details />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</MenuProvider>
 	);
 };
 export default App;
