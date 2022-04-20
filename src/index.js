f59025a833a2970f8855287e52a5f439755b200e
Smael Nicolas <nico.smael@gmail.com>
2022-04-19 22:19:40 -0300

    move context to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Index.css";
+import IsLoggedContextProvider from "./Context/IsLoggedContext";
+import { MenuProvider } from "./Context/MenuContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+	<IsLoggedContextProvider>
+		<MenuProvider>
+			<App />
+		</MenuProvider>
+	</IsLoggedContextProvider>
+);
