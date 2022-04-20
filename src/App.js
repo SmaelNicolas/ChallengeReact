0bec9ae7c2ea5dacfa80411394ad68fb25acb289
Smael Nicolas <nico.smael@gmail.com>
2022-04-19 22:19:56 -0300

    redirects and handle what to show

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import NavbarSection from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Search from "./Pages/Search/Search";
 import Details from "./Pages/Details/Details";
-import { MenuProvider } from "./Context/MenuContext";
+import { IsLoggedContext } from "./Context/IsLoggedContext";
 
 const App = () => {
+	const { getIsLogged } = useContext(IsLoggedContext);
+
 	return (
-		<MenuProvider>
-			<BrowserRouter>
-				<div className='appContainer'>
-					<NavbarSection />
-					<Routes>
-						<Route exact path='/' element={<Login />} />
+		<BrowserRouter>
+			<div className='appContainer'>
+				<NavbarSection />
+				<Routes>
+					<Route
+						path='/'
+						element={
+							getIsLogged() ? <Navigate to='/home' /> : <Login />
+						}
+					/>
+					<Route path='*' element={<Navigate to='/' />} />
+					{getIsLogged() && (
 						<Route exact path='/home' element={<Home />} />
+					)}
+					{getIsLogged() && (
 						<Route exact path='/search' element={<Search />} />
-						<Route exact path='/detail/:id' element={<Details />} />
-					</Routes>
-				</div>
-			</BrowserRouter>
-		</MenuProvider>
+					)}
+					{getIsLogged() && (
+						<Route
+							exact
+							path='/detail/:idRecipe'
+							element={<Details />}
+						/>
+					)}
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 };
 export default App;
