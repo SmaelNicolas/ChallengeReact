fe3bd0312c04eb55d5cfe2c30539cf0b59936b31
Smael Nicolas <nico.smael@gmail.com>
2022-04-27 17:06:02 -0300

    fix price per serving

--- src/Helpers/createCompleteRecipe.js
+++ src/Helpers/createCompleteRecipe.js
@@ -9,6 +9,7 @@ const createCompleteRecipe = async (recipes, fn, show) => {
 					`https://api.spoonacular.com/recipes/${rec.id}/information?apiKey=0aa9b30d89cc43f3968c6c1321dee967`
 				)
 				.then((response) => {
+					console.log(rec.id);
 					let data = response.data;
 					let completeRecipe = {
 						id: data.id,
@@ -17,13 +18,14 @@ const createCompleteRecipe = async (recipes, fn, show) => {
 						description: data.summary,
 						vegan: data.vegan,
 						helthScore: data.healthScore,
-						price: data.pricePerServing / data.servings,
+						price: data.pricePerServing / 100,
 						time: data.readyInMinutes,
 					};
 					arrayRecipes.push(completeRecipe);
 				})
 				// .catch((error) => )
 				.finally(() => {
+					console.log(arrayRecipes);
 					fn(arrayRecipes);
 					show();
 				});
