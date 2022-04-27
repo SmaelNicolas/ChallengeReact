c16a805e922cf8a6b30063ea9f0cb53063113235
Smael Nicolas <nico.smael@gmail.com>
2022-04-27 18:06:15 -0300

    item detail format

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,23 +1,61 @@
+import { Paper } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Title from "../../Components/Title/Title";
 import { getListRecipesSearched } from "../../Data/recipesSearched";
+import Loading from "../../Components/Loading/Loading";
+import "./Details.css";
 
 const Details = () => {
 	const { idRecipe } = useParams();
 
 	const [recipe, setRecipe] = useState();
+	const [loadingBoolean, setLoadingBoolean] = useState(true);
 
 	useEffect(() => {
-		let getRec = getListRecipesSearched().find(
-			(recipe) => recipe.id === idRecipe
+		let listRecipes = getListRecipesSearched();
+		let getRec = listRecipes.find(
+			(recipe) => recipe.id === parseInt(idRecipe)
 		);
 		setRecipe(getRec);
+		setTimeout(() => {
+			setLoadingBoolean(false);
+		}, 1500);
+	}, [idRecipe, recipe]);
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [idRecipe]);
-
-	return <Title>Details</Title>;
+	return (
+		<>
+			<Title title='details' />
+			{loadingBoolean ? (
+				<Loading />
+			) : (
+				<div className='detailsContainer'>
+					<h2>{recipe.title}</h2>
+					<img
+						className='detailImg'
+						src={recipe.img}
+						alt='img food'
+					/>
+					<ul className='infoContainer'>
+						<li>Price per serving : ${recipe.price.toFixed(2)}</li>
+						<li> Time preparation : {recipe.time}</li>
+						<li>Helth Score : {recipe.helthScore}</li>
+						<li>
+							Food Type : {recipe.vegan ? "Vegan" : "No Vegan"}
+						</li>
+					</ul>
+					<Paper elevation={24}>
+						<p
+							className='detailDescription'
+							dangerouslySetInnerHTML={{
+								__html: recipe.description,
+							}}
+						></p>
+					</Paper>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default Details;
