5a04e366b7765cdbd8658e4ea27805e488e6d903
Smael Nicolas <nico.smael@gmail.com>
2022-04-27 18:06:04 -0300

    add msg

--- src/Helpers/getSearchQuery.js
+++ src/Helpers/getSearchQuery.js
@@ -5,9 +5,11 @@ const doSearchQuery = async (value, fn) => {
 	await axios({
 		method: "GET",
 		url: `https://api.spoonacular.com/recipes/autocomplete?number=15&query=${value}&apiKey=0aa9b30d89cc43f3968c6c1321dee967 `,
-	}).then((response) => {
-		fn(response.data);
-	});
+	})
+		.then((response) => {
+			fn(response.data);
+		})
+		.catch(fn(null));
 	return result;
 };
 
